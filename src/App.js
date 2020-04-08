96d00a418bd3aa4b17ad7b0c9d36fe7fdefc4290
Sergei Reutov <basic.mehmat@gmail.com>
2020-04-08 20:20:48 +0200

    ESLint, Husky, Tests

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div data-testid="mainApp" className="App">
       <Router>
         <Switch>
           <Route exact path="/">
---
+++ src/utils/test-utils.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from 'reducers';
+
+function render(
+  ui,
+  {
+    initialState = {},
+    store = createStore(reducer, initialState),
+    ...renderOptions
+  } = {}
+) {
+  // eslint-disable-next-line
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { render };
